5b331ba797a8462fb5a4ea700a3f51b07564d757
Matthew Ya <matthew_ya@yahoo.com>
2016-09-26 20:02:51 -0400

    first commit

---
+++ SongLib/src/application/SongLib.java
@@ -0,0 +1,33 @@
+package application;
+//Matthew Ya, Taehee Lee	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import view.SongLibController;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+
+
+public class SongLib extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader();   
+			loader.setLocation(getClass().getResource("/view/SongLibUI.fxml"));
+			AnchorPane root = (AnchorPane)loader.load();
+
+			SongLibController listController = loader.getController();
+			listController.start(primaryStage);
+
+			Scene scene = new Scene(root, root.getPrefWidth(), root.getPrefHeight());
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ SongLib/src/view/SongLibController.java
@@ -0,0 +1,15 @@
+package view;
+
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.stage.Stage;
+
+public class SongLibController {
+	 @FXML ListView<String> listView;
+	 private ObservableList<String> obsList;
+	 
+	 public void start(Stage mainStage){
+		 
+	 }
+}
